ae3fac5845fb61a88550b98ad96cf2f929f69476
Michael Golbraikh <mgolbraikh@gmail.com>
2017-02-04 17:20:58 +0200

    my wines passing info to wine frag

--- Winez/app/src/main/java/com/example/owner/winez/MyWinesListFragment.java
+++ Winez/app/src/main/java/com/example/owner/winez/MyWinesListFragment.java
@@ -1,13 +1,18 @@
 package com.example.owner.winez;
 
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -15,6 +20,7 @@ import android.widget.TextView;
 
 import com.example.owner.winez.Model.Model;
 import com.example.owner.winez.Model.User;
+import com.example.owner.winez.Model.Wine;
 import com.example.owner.winez.Utils.WinezAuth;
 
 import java.util.List;
@@ -42,12 +48,27 @@ public class MyWinesListFragment extends Fragment {
         mAdapter = new MyWinesAdapter();
         list.setAdapter(mAdapter);
         list.setEmptyView(view.findViewById(R.id.mywines_empty_txt));
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Log.d("TAG", "row selected " + position);
+
+                Fragment wineDetailFrag = new WineFragment();
+                FragmentTransaction ftr  = getActivity().getFragmentManager().beginTransaction();
+                Bundle WineToShow = new Bundle();
+                WineToShow.putInt("WineId", (Integer)view.getTag());
+
+                wineDetailFrag.setArguments(WineToShow);
+                ftr.replace(R.id.WinezActivityMainView, wineDetailFrag);
+                ftr.addToBackStack(null);
+                ftr.show(wineDetailFrag);
+                ftr.commit();
+            }
+        });
+
         return view;
 
     }
-
-
-
 
     class MyWinesAdapter extends BaseAdapter{
         private User currentUser;
@@ -80,7 +101,7 @@ public class MyWinesListFragment extends Fragment {
                 removeImage.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        currentUser.getUserWines().remove(view.getTag());
+                        currentUser.getUserWines().remove(((View)view.getParent()).getTag());
                         notifyDataSetChanged();
                         Model.getInstance().saveCurrentUser(currentUser);
                     }
@@ -90,7 +111,7 @@ public class MyWinesListFragment extends Fragment {
             TextView text = (TextView) view.findViewById(R.id.mywines_row_text);
             text.setText(currentUser.getUserWines().get(item));
             ImageView removeImage = (ImageView) view.findViewById(R.id.mywines_remove_image);
-            removeImage.setTag(item);
+            view.setTag(item);
             return view;
         }
     }
